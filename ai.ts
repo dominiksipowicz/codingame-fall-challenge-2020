dd9c35d22c98651bc973b85535bb9c3f037259fd
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2020-11-15 02:31:24 +0000

    refactoring, debugging, filterAfordableRecipies

--- ai.ts
+++ ai.ts
@@ -5,21 +5,38 @@ const initAction = {
 
 type actionTypes = "BREW" | "CAST";
 
-type Recipe = {
+type Order = {
     actionId: number;
     delta: number[];
     price: number;
 };
 
-let orders: Recipe[] = [];
-let spells: Recipe[] = [];
+type Spell = {
+    actionId: number;
+    delta: number[];
+};
 
-const filterRecipies = (recipes: Recipe[], myInventoryDelta): Recipe[] => {
+let orders: Order[] = [];
+let spells: Spell[] = [];
+let userData: any[];
+
+type Recipes = (Order | Spell)[];
+
+const filterRecipies = (recipes: Recipes, myInventoryDelta: number[]): Recipes => {
   return recipes.filter(recipe => {
-      if (recipe.delta[0] + myInventoryDelta[0] < 0) return false;
-      if (recipe.delta[1] + myInventoryDelta[1] < 0) return false;
-      if (recipe.delta[2] + myInventoryDelta[2] < 0) return false;
-      if (recipe.delta[3] + myInventoryDelta[3] < 0) return false;
+      console.error(`====`);
+      console.error(`recipe:` + recipe.delta);
+      console.error(`myInventoryDelta:` + myInventoryDelta);
+      console.error(`suma0 = ` + (recipe.delta[0] + myInventoryDelta[0]));
+      console.error(`suma1 = ` + (recipe.delta[1] + myInventoryDelta[1]));
+      console.error(`suma2 = ` + (recipe.delta[2] + myInventoryDelta[2]));
+      console.error(`suma3 = ` + (recipe.delta[3] + myInventoryDelta[3]));
+
+      if ((recipe.delta[0] + myInventoryDelta[0]) < 0) return false;
+      if ((recipe.delta[1] + myInventoryDelta[1]) < 0) return false;
+      if ((recipe.delta[2] + myInventoryDelta[2]) < 0) return false;
+      if ((recipe.delta[3] + myInventoryDelta[3]) < 0) return false;
+      return true;
   });
 }
 
@@ -32,6 +49,7 @@ while (true) {
     nextAction = initAction;
     orders = [];
     spells = [];
+    userData = [];
 
     const actionCount: number = parseInt(readline()); // the number of spells and recipes in play
 
@@ -60,29 +78,36 @@ while (true) {
         if (actionType == "CAST") {
             spells.push({
                 actionId,
-                delta: [delta0, delta1, delta2, delta3],
-                price
+                delta: [delta0, delta1, delta2, delta3]
             });
         }
     }
-    // for (let i = 0; i < 1; i++) { // i can be two. TODO: check oponent inventory or points.
+
+
+    for (let i = 0; i < 2; i++) {
         var inputs: string[] = readline().split(' ');
         const inv0: number = parseInt(inputs[0]); // tier-0 ingredients in inventory
         const inv1: number = parseInt(inputs[1]);
         const inv2: number = parseInt(inputs[2]);
         const inv3: number = parseInt(inputs[3]);
         const score: number = parseInt(inputs[4]); // amount of rupees
-    // }
 
-    const myInventoryDelta = [
-      inv0,
-      inv1,
-      inv2,
-      inv3
-    ];
-    const myScore = score;
+        const inventoryDelta: number[] = [
+          inv0,
+          inv1,
+          inv2,
+          inv3
+        ];
 
-    const afordableOrders = filterRecipies(orders, myInventoryDelta);
+        const userDataRow = {
+          inventoryDelta,
+          score
+        }
+        userData.push(userDataRow)
+    }
+
+    const afordableOrders = filterRecipies(orders, userData[0].inventoryDelta) as Order[];
+    const afordableSpells = filterRecipies(spells, userData[0].inventoryDelta) as Spell[];
 
     if (afordableOrders.length > 0) {
       afordableOrders.forEach(element => {
@@ -95,18 +120,24 @@ while (true) {
       });
     }
 
-    const spellsAvailable = spells.length;
-
-    if (nextAction.price) {
+    if (nextAction.price > 0) {
       // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
       console.log('BREW ' + nextAction.actionId);
+    } else if (afordableSpells.length > 0) {
+      console.log('CAST ' + afordableSpells[0].actionId);
+    } else {
+      console.log('REST');  // what's a different between REST and WAIT?
+
     }
 
 
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
     console.error(`=========`);
-    console.error(afordableOrders);
+    console.error(`afordableOrders: ` + afordableOrders.length);
+    console.error(`spellsAvailable: ` + spells.length);
+    console.error(`afordableSpells: ` + afordableSpells.length);
+    console.error(`myInventoryDelta: ` + userData[0].inventoryDelta);
     console.error(`=========`);
 
 
