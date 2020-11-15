64e5eafcc409ff6e6bb9b447a2b7f077c54df225
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2020-11-15 01:25:03 +0000

    add filterRecipies and afordableOrders

--- ai.ts
+++ ai.ts
@@ -11,16 +11,29 @@ type Recipe = {
     price: number;
 };
 
-let recipes = [];
+let orders: Recipe[] = [];
+let spells: Recipe[] = [];
 
+const filterRecipies = (recipes: Recipe[], myInventoryDelta): Recipe[] => {
+  return recipes.filter(recipe => {
+      if (recipe.delta[0] + myInventoryDelta[0] < 0) return false;
+      if (recipe.delta[1] + myInventoryDelta[1] < 0) return false;
+      if (recipe.delta[2] + myInventoryDelta[2] < 0) return false;
+      if (recipe.delta[3] + myInventoryDelta[3] < 0) return false;
+  });
+}
 
 let nextAction = initAction;
 
 // game loop
 while (true) {
-    const actionCount: number = parseInt(readline()); // the number of spells and recipes in play
+
+    // reset variables
     nextAction = initAction;
-    recipes = [];
+    orders = [];
+    spells = [];
+
+    const actionCount: number = parseInt(readline()); // the number of spells and recipes in play
 
     for (let i = 0; i < actionCount; i++) {
         var inputs: string[] = readline().split(' ');
@@ -37,34 +50,64 @@ while (true) {
         const repeatable: boolean = inputs[10] !== '0'; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
 
         if (actionType == "BREW") {
-            recipes.push({
+            orders.push({
                 actionId,
                 delta: [delta0, delta1, delta2, delta3],
                 price
             });
         }
 
-        if (price > nextAction.price ) {
-            nextAction = {
+        if (actionType == "CAST") {
+            spells.push({
                 actionId,
+                delta: [delta0, delta1, delta2, delta3],
                 price
-            }
+            });
         }
     }
-    for (let i = 0; i < 2; i++) {
+    // for (let i = 0; i < 1; i++) { // i can be two. TODO: check oponent inventory or points.
         var inputs: string[] = readline().split(' ');
         const inv0: number = parseInt(inputs[0]); // tier-0 ingredients in inventory
         const inv1: number = parseInt(inputs[1]);
         const inv2: number = parseInt(inputs[2]);
         const inv3: number = parseInt(inputs[3]);
         const score: number = parseInt(inputs[4]); // amount of rupees
+    // }
+
+    const myInventoryDelta = [
+      inv0,
+      inv1,
+      inv2,
+      inv3
+    ];
+    const myScore = score;
+
+    const afordableOrders = filterRecipies(orders, myInventoryDelta);
+
+    if (afordableOrders.length > 0) {
+      afordableOrders.forEach(element => {
+        if (element.price > nextAction.price ) {
+            nextAction = {
+                actionId: element.actionId,
+                price: element.price
+            }
+        }
+      });
     }
+
+    const spellsAvailable = spells.length;
+
+    if (nextAction.price) {
+      // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
+      console.log('BREW ' + nextAction.actionId);
+    }
+
 
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
-    console.error(recipes);
+    console.error(`=========`);
+    console.error(afordableOrders);
     console.error(`=========`);
 
-    // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-    console.log('BREW ' + nextAction.actionId);
+
 }
